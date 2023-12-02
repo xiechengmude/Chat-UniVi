ec1e5d3a118f1cf26b6eeb758a4c740b943a0160
xiechengmude <xdan_dev@163.com>
2023-12-02 23:59:48 +0800

    add xDAN Training Script

---
+++ xDAN-Pretrain-6b.py
@@ -0,0 +1,35 @@
+deepspeed \
+--include localhost:0,1,2,3,4,5,6,7 \
+--master_port=29602 \
+ChatUniVi/train/train_mem.py \
+--deepspeed scripts/zero3.json \
+--model_name_or_path ${LLM model path} \
+--version v1 \
+--model_use PRETUNE \
+--dataset_use Pretrain \
+--vision_tower openai/clip-vit-large-patch14 \
+--tune_mm_mlp_adapter True \
+--mm_vision_select_layer -2 \
+--mm_use_im_start_end False \
+--mm_use_im_patch_token False \
+--bf16 True \
+--output_dir ${stage1 save path} \
+--num_train_epochs 1 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 4 \
+--gradient_accumulation_steps 1 \
+--evaluation_strategy "no" \
+--save_strategy "steps" \
+--save_steps 24000 \
+--save_total_limit 1 \
+--learning_rate 2e-3 \
+--weight_decay 0. \
+--warmup_ratio 0.03 \
+--lr_scheduler_type "cosine" \
+--logging_steps 1 \
+--tf32 True \
+--model_max_length 2048 \
+--gradient_checkpointing True \
+--dataloader_num_workers 4 \
+--lazy_preprocess True \
+--report_to wandb
