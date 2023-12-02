414ca71918189b180fae38d2e3bf09c9eb3649bc
xiechengmude <xdan_dev@163.com>
2023-12-02 23:57:18 +0800

    specific the path

--- ChatUniVi/config/dataset_config.py
+++ ChatUniVi/config/dataset_config.py
@@ -1,41 +1,41 @@
 Pretrain = {
-    "chat_path": "${PATH}/CC3M-595K/chat.json",
-    "CC3M": "${PATH}/CC3M-595K",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Pre-training/CC3M-595K/chat.json",
+    "CC3M": "/workspace/Chat-UniVi/dataset/Pre-training/CC3M-595K",
 }
 
 VIT = {
-    "chat_path": "${PATH}/llava_instruct_150k.json",
-    "COCO2017": "${PATH}/COCO2017/train2017",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Fine-tuning/VIT/llava_instruct_150k.json",
+    "COCO2017": "/workspace/Chat-UniVi/dataset/Fine-tuning/VIT/COCO2017/train2017",
 }
 
 MIMIC_imageonly = {
-    "chat_path": "${PATH}/MIMIC-IT-imageonly.json",
-    "CDG": "${PATH}/CGD/images",
-    "LA": "${PATH}/LA/images",
-    "SD": "${PATH}/SD/images",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Fine-tuning/MIMIC_imageonly/MIMIC-IT-imageonly.json",
+    "CGD": "/workspace/Chat-UniVi/dataset/Fine-tuning/MIMIC_imageonly/CGD/images",
+    "LA": "/workspace/Chat-UniVi/dataset/Fine-tuning/MIMIC_imageonly/LA/images",
+    "SD": "/workspace/Chat-UniVi/dataset/Fine-tuning/MIMIC_imageonly/SD/images",
 }
 
 COCO_CAP = {
-    "chat_path": "${PATH}/COCO/coco_cap_chat.json",
-    "COCO2014": "${PATH}/COCO2014/train2014",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/coco_cap_chat.json",
+    "COCO2014": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/train2014",
 }
 
 COCO_REG = {
-    "chat_path": "${PATH}/COCO/coco_reg_chat.json",
-    "COCO2014": "${PATH}/COCO2014/train2014",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/coco_reg_chat.json",
+    "COCO2014": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/train2014",
 }
 
 COCO_REC = {
-    "chat_path": "${PATH}/COCO/coco_rec_chat.json",
-    "COCO2014": "${PATH}/COCO2014/train2014",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/coco_rec_chat.json",
+    "COCO2014": "/workspace/Chat-UniVi/dataset/Pre-training/COCO/train2014",
 }
 
 VIDEO = {
-    "chat_path": "${PATH}/video_chat.json",
-    "VIDEO": "${PATH}/Activity_Videos",
+    "chat_path": "/workspace/Chat-UniVi/dataset/Fine-tuning/VIDEO/video_chat.json",
+    "VIDEO": "/workspace/Chat-UniVi/dataset/Fine-tuning/VIDEO/Activity_Videos",
 }
 
 SQA = {
-    "chat_path": "${PATH}/llava_train_QCM-LEA.json",
-    "ScienceQA": "${PATH}/scienceqa/train",
+    "chat_path": "/workspace/Chat-UniVi/dataset/ScienceQA_tuning/llava_train_QCM-LEA.json",
+    "ScienceQA": "/workspace/Chat-UniVi/dataset/ScienceQA_tuning/train",
 }
